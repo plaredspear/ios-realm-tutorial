febc30051521f6dbc6025951c2917e717dd1fbf6
plaredspear <plaredspear@gmail.com>
2016-04-07 12:24:16 +0900

    TableView 구성

--- realm-tutorial/ViewController.swift
+++ realm-tutorial/ViewController.swift
@@ -8,11 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    @IBOutlet var textInput: UITextField!
+    
+    var array = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        array.append("WOWO")
+        array.append("WOWO2")
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +26,28 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.array.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let row = self.array[indexPath.row]
+        let cell = tableView.dequeueReusableCellWithIdentifier("ListCell") as UITableViewCell!
+        cell.textLabel?.text = row
+        return cell
+    }
 
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+    }
+    
+    @IBAction func addText(sender: AnyObject) {
+        if let text = textInput.text {
+            array.append(text)
+            textInput.text = ""
+        }
+        
+        self.tableView.reloadData()
+    }
 }
 
